8061126104b55e374134aa7eb670f7db5194473a
Bogdan <springtime4hitler@yandex.ru>
2022-10-28 01:40:07 +0300

    first commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponse
-from django.db.models import Sum, F
 from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
